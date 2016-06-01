df6fb1fe1548345da8b58d953d20392f9563da6f
Evgeniy Kosjakov <42_@mail.ru>
2016-06-01 14:20:38 +0300

    badge small refactor

--- TopRatedApp/Common/Badges/BadgeBase.cs
+++ TopRatedApp/Common/Badges/BadgeBase.cs
@@ -4,13 +4,42 @@ namespace TopRatedApp.Common.Badges
 {
     public class BadgeBase
     {
-        public BadgeBase(Section[] sections, IBadgeStyle style)
+        private string GetBackgroundColor(string theme)
         {
-            Sections = sections;
-            Style = style;
+            switch (theme)
+            {
+                case "light":
+                    return Color.Silver;
+                case "dark":
+                    return Color.DarkGrey;
+                default:
+                    return Color.Silver;
+            }
         }
 
-        public Section[] Sections { get; }
-        public IBadgeStyle Style { get; }
+        public BadgeBase(string theme = "")
+        {
+            Theme = theme.Equals("") ? "light" : theme;
+            Sections = new Section[] {};
+        }
+
+        public BadgeBase(Section[] sections, string theme = "")
+        {
+            Theme = theme.Equals("") ? "light" : theme;
+            Sections = sections;
+        }
+
+        public Section[] Sections { get; set; }
+
+        public string Theme { get; }
+        public IFontStyle FontStyle => new FontStyle(
+            "DejaVu Sans,Verdana,Geneva,sans-serif", 
+            "11", 
+            "", 
+            "");
+        public ISectionStyle CommonTextStyle => new SectionStyle(
+            FontStyle, 
+            "");
+        public IBadgeStyle Style => new BadgeStyle(CommonTextStyle, "5", "3", "4", "4");
     }
 }
--- TopRatedApp/Common/Badges/LanguageBadge.cs
+++ TopRatedApp/Common/Badges/LanguageBadge.cs
@@ -1,16 +1,17 @@
-﻿using TopRatedApp.Interfaces;
+﻿using TopRatedApp.Enums;
+using TopRatedApp.Interfaces;
 
 namespace TopRatedApp.Common.Badges
 {
     public class LanguageBadge : BadgeBase
     {
-        private static readonly IFontStyle FontStyle = new FontStyle("", "11", "", "");
-        private static readonly ISectionStyle CommonTextStyle = new SectionStyle(FontStyle, "");
-        private static readonly IBadgeStyle BadgeStyle = new BadgeStyle(CommonTextStyle, "5", "3", "4", "4");
-
-        public LanguageBadge(Section[] sections) : base(sections, BadgeStyle)
+        public LanguageBadge(ILanguage language)
         {
-
+            Sections = new []
+            {
+                new Section("language", SectionType.Text, CommonTextStyle),
+                new Section(language.Name, language.SectionType, new SectionStyle(FontStyle, language.Color))
+            };
         }
     }
 }
